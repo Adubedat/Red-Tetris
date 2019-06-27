d8d5816e86876bae6fddbd6771950f73edcb49a1
Arthur Dubedat <arthur.dubedat@gmail.com>
2019-06-27 15:34:13 +0200

    Login test in progress

--- src/client/components/Login/index.jsx
+++ src/client/components/Login/index.jsx
@@ -1,38 +1,10 @@
-import React, { useState } from "react";
 import { connect } from "react-redux";
 import { connectPlayer } from "../../actions/player";
 import LoginSub from "./subcomponent";
-import inputError from "../../errors/inputError";
-
-export const Login = ({ connectPlayer }) => {
-  const [error, setError] = useState({ boolean: null, message: "" });
-  const [playerName, setplayerName] = useState("");
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!error.boolean && playerName) connectPlayer(playerName);
-  };
-
-  const handleChange = e => {
-    const value = e.target.value;
-    setError(inputError(value));
-    setplayerName(value);
-  };
-
-  return (
-    <LoginSub
-      onSubmit={handleSubmit}
-      error={error}
-      value={playerName}
-      label="Name"
-      onChange={handleChange}
-    />
-  );
-};
 
 const actionCreators = { connectPlayer };
 
 export default connect(
   null,
   actionCreators
-)(Login);
+)(LoginSub);
