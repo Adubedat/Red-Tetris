bfe067ff310a52e881cbad37aad07cfd5bdd9c08
Arthur Dubedat <arthur.dubedat@gmail.com>
2019-07-03 17:03:00 +0200

    debug copy array

--- src/server/player/class.js
+++ src/server/player/class.js
@@ -39,18 +39,17 @@ class Player {
   }
 
   printPieceToBoard() {
-    console.log("before: ", this._board);
     const [y, x] = this._currentPiece.pos;
     const shape = this._currentPiece.shape;
-    let fakeBoard = [...this._board];
+    let newBoard = this._board.map(row => {
+      return [...row];
+    });
     for (let i = 0; i < shape.length; i++) {
       for (let j = 0; j < shape[i].length; j++) {
-        if (shape[i][j]) fakeBoard[i + y][j + x] = 1;
+        if (shape[i][j]) newBoard[i + y][j + x] = 1;
       }
     }
-    console.log("after: ", this._board);
-
-    return fakeBoard;
+    return newBoard;
   }
 
   toObject() {
