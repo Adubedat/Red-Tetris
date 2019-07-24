d3d1292cdda3e34f955d848b5453176c2bd75082
42plamusse <plamusse42@gmail.com>
2019-07-24 10:01:28 +0200

    http try

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const http = require("http");
-const https = require("https");
-const fs = require("fs");
 const path = require("path");
 const socketIO = require("socket.io");
 const { initListeners } = require("./src/server/listeners/index.js");
 const params = require("./params");
 
 let port = process.env.PORT;
-if (port === null || port === "") {
+if (port == null || port == "") {
   port = params.server.port;
 }
 
@@ -21,23 +19,12 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const httpsServer = https.createServer(
-  {
-    key: fs.readFileSync("key.pem"),
-    cert: fs.readFileSync("cert.pem")
-  },
-  app
-);
-// const httpServer = http.createServer(app);
+const server = http.createServer(app);
 
-const io = socketIO(httpsServer, { pingInterval: 60000 });
+const io = socketIO(server, { pingInterval: 60000 });
 
 initListeners(io);
 
-// httpServer.listen(port, () => {
-//   console.log(`Listening on port ${port}`);
-// });
-
-httpsServer.listen(port, () => {
+server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
