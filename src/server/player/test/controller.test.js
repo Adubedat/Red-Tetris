0864a4d92277db8c621f3b09e7107b75d1a05a05
Arthur Dubedat <arthur.dubedat@gmail.com>
2019-11-25 15:08:40 +0100

    Game and player controllers tests done

--- src/server/player/test/controller.test.js
+++ src/server/player/test/controller.test.js
@@ -53,19 +53,16 @@ afterEach(done => {
 });
 
 describe("Test player controller functions", () => {
-  test("connectPlayer function with an unvalid name should callback an error", () => {
-    const callback = jest.fn(response => {
-      expect(response.status).toBe("error");
+  test("connectPlayer function with an unvalid name should do nothing", done => {
+    socket.emit(NEW_PLAYER, "Player1unvalid");
+    setTimeout(() => {
       expect(Game.players.length).toBe(0);
-    });
-    socket.emit(NEW_PLAYER, "Player1unvalid", callback);
+      done();
+    }, 50);
   });
   test("connectPlayer with a valid name should create a player and do nothing when the player already exists ", done => {
-    const callback = jest.fn(response => {
-      expect(response.status).toBe("success");
-    });
-    socket.emit(NEW_PLAYER, "Player1", callback);
-    socket.emit(NEW_PLAYER, "Player1", callback);
+    socket.emit(NEW_PLAYER, "Player1");
+    socket.emit(NEW_PLAYER, "Player1");
     setTimeout(() => {
       expect(Game.players.length).toBe(1);
       expect(Game.players[0].id).toBe(socket.id);
@@ -73,8 +70,7 @@ describe("Test player controller functions", () => {
     }, 50);
   });
   test("disconnectPlayer should remove player from game and from the socket room LOBBY_ROOM", done => {
-    const callback = jest.fn(() => {});
-    socket.emit(NEW_PLAYER, "Player1", callback);
+    socket.emit(NEW_PLAYER, "Player1");
     socket.emit(DISCONNECT_PLAYER);
     setTimeout(() => {
       expect(Game.players.length).toBe(0);
@@ -82,8 +78,7 @@ describe("Test player controller functions", () => {
     }, 50);
   });
   test("updatePlayerClientSide should emit an UPDATE_PLAYER event", done => {
-    const callback = jest.fn(() => {});
-    socket.emit(NEW_PLAYER, "Player1", callback);
+    socket.emit(NEW_PLAYER, "Player1");
     socket.on(UPDATE_PLAYER, data => {
       expect(data).toBeDefined();
       done();
